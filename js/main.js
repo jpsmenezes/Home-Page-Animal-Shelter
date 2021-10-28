0304c0e6d435cdd61aaeabda02948dc3acb4809a
João Paulo Menezes <joaopaulo.smenezes@hotmail.com>
2021-10-28 11:08:19 -0300

    js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-//Efeito de scroll no menu
+/*////////////////////////////// EFEITO DE SCROLL NO HEADER ////////////////////////////////////*/
 window.addEventListener('scroll', function(){
     const header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
 })
 
-//Efeito Parallax na imagem do banner
+/*/////////////////////////// EFEITO PARALLAX NA IMAGEM DO HERO /////////////////////////////////*/
 const image = document.querySelector('#imageParallax');
 window.addEventListener('scroll',function(){
     const value = window.scrollY;
     image.style.top = value * 0.5 + "px"; 
 })
 
-// Menu toggle
+/*/////////////////////////////// ICON DE MENU / MENU TOGGLE /////////////////////////////////////*/
 let show = true;
 
 const menuSection = document.querySelector('.menu-section');
@@ -24,3 +24,21 @@ menuToggle.addEventListener('click', () => {
     show = !show;
 })
 
+/*//////////////////////////////////////// ANIMAÇÃO //////////////////////////////////////////////*/
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll(){
+    const windowTop = window.scrollY + ((window.innerHeight * 3) / 4);
+    target.forEach(function(element){        
+        if(windowTop > element.offsetTop){
+            element.classList.add(animationClass);
+        }else{
+            element.classList.remove(animationClass);
+        }
+    })
+}
+animeScroll()
+window.addEventListener('scroll', function(){
+    animeScroll()   
+})
